87003f4f3450ee3f7bc30fbfe426a54babb4056d
AoiDeveloper <76095792+AoiDeveloper@users.noreply.github.com>
2021-12-10 23:14:22 +0900

    Add TestGun

--- src/main/java/com/gmail/aoibotowner/smartgun/SmartGun.java
+++ src/main/java/com/gmail/aoibotowner/smartgun/SmartGun.java
@@ -1,17 +1,18 @@
 package com.gmail.aoibotowner.smartgun;
 
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class SmartGun extends JavaPlugin {
+    private static SmartGun plugin;
 
     @Override
     public void onEnable() {
         // Plugin startup logic
-
+        plugin = this;
     }
 
-    @Override
-    public void onDisable() {
-        // Plugin shutdown logic
+    public static Plugin getPlugin() {
+        return plugin;
     }
 }
